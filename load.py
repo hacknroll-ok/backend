097e9271aca70ce5f2c33cfb26ac82323901fb7b
ppv-1 <virus9320@gmail.com>
2025-01-18 15:15:36 +0800

    update load.py

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 # Define parameters
 dataset_dir = "./dataset"  # Replace with the path to your dataset
 image_size = (28, 28)  # Example image size
@@ -27,3 +27,14 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+plt.figure(figsize=(8, 8))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        data = images[i].numpy().astype("uint8")
+        plt.imshow(data, cmap='gray', vmin=0, vmax=255)
+        plt.title(train_ds.class_names[labels[i]])
+        plt.axis("off")
+
+print(f"Training dataset size: {len(train_ds)} batches")
+print(f"Validation dataset size: {len(val_ds)} batches")
